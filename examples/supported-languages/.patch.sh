9ece9fefb30f67f3e65b1679c6867e5be8c33c91
Sander <hey@sandydoo.me>
2024-06-02 13:11:57 +0400

    examples: disable odin on macos in CI

--- examples/supported-languages/.patch.sh
+++ examples/supported-languages/.patch.sh
@@ -4,4 +4,6 @@ echo "  languages.unison.enable = lib.mkForce (!(pkgs.stdenv.isLinux && pkgs.std
 echo "  languages.standardml.enable = lib.mkForce (!pkgs.stdenv.isAarch64);" >> devenv.local.nix
 # https://github.com/NixOS/nixpkgs/issues/297019
 echo "  languages.purescript.enable = lib.mkForce (!pkgs.stdenv.isAarch64);" >> devenv.local.nix
+# Doesn't build on macOS. Check nixpkgs
+echo "  languages.odin.enable = lib.mkForce (!pkgs.stdenv.isDarwin);" >> devenv.local.nix
 echo "}" >> devenv.local.nix
