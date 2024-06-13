16961546eeaaa8d0f3c49a23e98b0286f1d15171
Sander <hey@sandydoo.me>
2024-06-13 14:17:29 +0000

    example: fixup phoenix example

--- examples/phoenix/.setup.sh
+++ examples/phoenix/.setup.sh
@@ -1,9 +1,15 @@
-set +x
+#!/usr/bin/env bash
+set -ex
 
 mix local.hex --force
 mix local.rebar --force
 mix archive.install --force hex phx_new
 
 echo y | mix phx.new --install hello
-sed -i.bak -e "s/hostname: \"localhost\"/socket_dir: System.get_env(\"PGHOST\")/" \
-  ./hello/config/dev.exs && rm ./hello/config/dev.exs.bak
+
+pushd hello
+  sed -i.bak -e "s/hostname: \"localhost\"/socket_dir: System.get_env(\"PGHOST\")/" \
+    ./config/dev.exs && rm ./config/dev.exs.bak
+
+  mix deps.get
+popd
