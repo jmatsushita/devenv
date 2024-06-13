a35e90e067abc71995e0d5a60afa894b9ee74139
Sander <hey@sandydoo.me>
2024-06-13 23:56:41 +0400

    example: install phoenix deps separately

--- examples/phoenix/.setup.sh
+++ examples/phoenix/.setup.sh
@@ -5,8 +5,11 @@ mix local.hex --force
 mix local.rebar --force
 mix archive.install --force hex phx_new
 
-echo y | mix phx.new --install hello
+if [ ! -d "hello" ]; then
+  yes | mix phx.new hello
+  sed -i -e "s/hostname: \"localhost\"/socket_dir: System.get_env(\"PGHOST\")/" ./hello/config/dev.exs
+fi
 
 pushd hello
-  sed -i -e "s/hostname: \"localhost\"/socket_dir: System.get_env(\"PGHOST\")/" ./config/dev.exs
+  mix deps.get
 popd
